0cec55a3659a650b899b6a51d5659a588debb3a0
guillermina <suttersg@gmail.com>
2022-01-20 15:48:06 -0500

    Data + d3 map update

--- hfiMap/hfiPopulation.py
+++ hfiMap/hfiPopulation.py
@@ -23,7 +23,7 @@ df = pd.DataFrame(rows, columns=['ISO2', 'ISO3', 'ISO3n', 'fips', 'country', 'ca
 df = df.iloc[1:, :]
 
 # keep columns to merge with datasets
-merge_pop = df[['ISO2', 'ISO3']]
+merge_pop = df[['ISO2', 'ISO3', 'country']]
 
 # read cities.csv
 # error: https://stackoverflow.com/questions/18171739/unicodedecodeerror-when-reading-csv-file-in-pandas-with-python
@@ -41,17 +41,20 @@ hfi_cities = merge_pop.merge(merge_hfi, how='left', left_on='ISO3', right_on='IS
 # left merge of merge_cities + hfi_cities
 final_df = hfi_cities.merge(merge_cities, how='left', left_on='ISO2', right_on='country code')
 
-# fill NaN with Zeros
+# fill quartile NaN with zeros, fill year NaN with 2019 to account for countries that are not in the HFI
 final_df['hf_quartile'] = final_df['hf_quartile'].fillna(0)
-print(final_df['ISO2'].nunique())
+final_df['year'] = final_df['year'].fillna(2019)
+
+# fill missing country names with countries from geonames
+final_df['countries'] = final_df['countries'].fillna(final_df['country'])
 
 # filter by year 2019
 df2019 = final_df.loc[final_df['year'] == 2019]
 
 # print rows with NaN values
-print(df2019[df2019.isna().any(axis=1)])
-
-# count Unique values - Count Countries
-print(df2019['countries'].nunique())
+# print(df2019[df2019.isna().any(axis=1)])
+#
+# # count Unique values - Count Countries
+# print(df2019['countries'].nunique())
 
 df2019.to_csv('cities2019.csv', index='reset')
