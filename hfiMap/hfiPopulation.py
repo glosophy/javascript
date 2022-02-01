3e8d02ebacafa427168601e845d19a2de0d0e809
glosophy <suttersg@gmail.com>
2022-01-31 21:05:52 -0500

    Data update

--- hfiMap/hfiPopulation.py
+++ hfiMap/hfiPopulation.py
@@ -1,6 +1,7 @@
 import bs4 as bs
 import urllib.request
 import pandas as pd
+
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 10000)
 pd.set_option('display.width', 1000)
@@ -22,6 +23,12 @@ df = df.iloc[1:, :]
 # keep columns to merge with datasets
 merge_pop = df[['ISO2', 'ISO3', 'country']]
 
+# Namibia cities
+namibia = pd.read_csv('na.csv')
+namibia = namibia.rename(columns={'city': 'asciiname', 'lat': 'latitude', 'lng': 'longitude',
+                                  'country': 'countries'})
+namibia = namibia.drop(['iso2', 'admin_name', 'capital', 'population_proper'], axis=1)
+
 # read cities.csv
 # error: https://stackoverflow.com/questions/18171739/unicodedecodeerror-when-reading-csv-file-in-pandas-with-python
 cities = pd.read_csv('cities15000.csv', encoding='latin')
@@ -38,20 +45,18 @@ hfi_cities = merge_pop.merge(merge_hfi, how='left', left_on='ISO3', right_on='IS
 # left merge of merge_cities + hfi_cities
 final_df = hfi_cities.merge(merge_cities, how='left', left_on='ISO2', right_on='country code')
 
+final = final_df.merge(namibia, how='left', left_on=['countries', 'population', 'latitude', 'longitude', 'asciiname'],
+                       right_on=['countries', 'population', 'latitude', 'longitude', 'asciiname'])
+print(final.head())
+
 # fill quartile NaN with zeros, fill year NaN with 2019 to account for countries that are not in the HFI
-final_df['hf_quartile'] = final_df['hf_quartile'].fillna(0)
-final_df['year'] = final_df['year'].fillna(2019)
+final['hf_quartile'] = final['hf_quartile'].fillna(0)
+final['year'] = final['year'].fillna(2019)
 
 # fill missing country names with countries from geonames
-final_df['countries'] = final_df['countries'].fillna(final_df['country'])
+final['countries'] = final['countries'].fillna(final['country'])
 
 # filter by year 2019
-df2019 = final_df.loc[final_df['year'] == 2019]
-
-# print rows with NaN values
-# print(df2019[df2019.isna().any(axis=1)])
-#
-# # count Unique values - Count Countries
-# print(df2019['countries'].nunique())
+df2019 = final.loc[final['year'] == 2019]
 
 df2019.to_csv('cities2019.csv', index='reset')
