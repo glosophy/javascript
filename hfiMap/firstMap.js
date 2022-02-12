a516627d6d5b520894b0ce5f6263703bfe931bc5
glosophy <suttersg@gmail.com>
2022-02-12 18:38:55 -0500

    d3 update map

--- hfiMap/firstMap.js
+++ hfiMap/firstMap.js
@@ -12,8 +12,8 @@ async function drawChart() {
     // 2. Create chart dimensions
 
     const width = d3.min([
-    window.innerWidth * 1.85,
-    window.innerHeight * 1.85,
+    window.innerWidth * 1.75,
+    window.innerHeight * 1.75,
     ])
 
     let dimensions = {
@@ -22,8 +22,8 @@ async function drawChart() {
         margin: {
             top: 10,
             right: 10,
-            bottom: 100,
-            left: 0,
+            bottom: 50,
+            left: 50,
         },
     }
 
