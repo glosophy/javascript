13833597f4da1b0658dadf968fa389f483b0ea82
glosophy <suttersg@gmail.com>
2022-07-28 22:00:13 -0400

    code-art project

--- code-art/projects/index.js
+++ code-art/projects/index.js
@@ -4,9 +4,17 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // console.log(ctx);
 
-canvas.addEventListener('mouseMove', function (e) {
-  ctx.beginPath();
-  ctx.rect(e.x, e.y, 10, 10);
-  ctx.fill();
-  // console.log(e.x, e.y);
-})
+// canvas.addEventListener('mousemove', function (e) {
+//   ctx.beginPath();
+//   ctx.rect(e.x, e.y, 10, 10);
+//   ctx.fill();
+//   // console.log(e.x, e.y);
+// })
+
+const degToRad = (deg) => {
+  return deg / 180 * Math.PI;
+};
+
+ctx.beginPath();
+ctx.arc(100, 100, 50, 0, degToRad(90));
+ctx.stroke();
